d34094289dfbab80f2ae1854f1157fe3e4d21466
predator112 <65210380+predator112@users.noreply.github.com>
2021-09-11 08:31:45 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+           git clone https://github.com/predator112/whyred-4.19 kernel
+           cd kernel
+           git clone https://github.com/PREDATOR-project/myscripts.git -b master
+           chmod +x myscripts/clang-setup.sh
+           chmod +x myscripts/ten-kernel.sh
+           source ./myscripts/clang-setup.sh
+           ./myscripts/ten-kernel.sh
